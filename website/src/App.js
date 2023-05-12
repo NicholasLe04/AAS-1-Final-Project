d8a99057706d85c70f5ddb2ae7eccd1001ba3735
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-05-12 15:44:51 -0700

    update events

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Dashboard from './components/Dashboard';
-import './App.css'
+import Dashboard from "./components/Dashboard";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <h1>ASIAN AMERICAN STUDIES</h1>
-      <Dashboard/>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <h1>ASIAN AMERICAN STUDIES</h1>
+            <Dashboard />
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- website/src/components/TimelineItem.js
+++ website/src/components/TimelineItem.js
@@ -1,37 +1,62 @@
-import React, { useEffect } from 'react';
-import './timeline.css';
+import React, { useEffect } from "react";
+import "./timeline.css";
 
 const Timeline = ({ events }) => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const events = document.querySelectorAll('.event');
-      events.forEach(event => {
-        const eventTop = event.getBoundingClientRect().top;
-        const eventBottom = event.getBoundingClientRect().bottom;
-        const windowHeight = window.innerHeight;
-        if (eventTop < windowHeight && eventBottom > 0) {
-          event.classList.add('active');
-        } else {
-          event.classList.remove('active');
-        }
-      });
-    };
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            const events = document.querySelectorAll(".event");
+            events.forEach((event) => {
+                const eventTop = event.getBoundingClientRect().top;
+                const eventBottom = event.getBoundingClientRect().bottom;
+                const windowHeight = window.innerHeight;
+                if (eventTop < windowHeight && eventBottom > 0) {
+                    event.classList.add("active");
+                } else {
+                    event.classList.remove("active");
+                }
+            });
+        };
+        window.addEventListener("scroll", handleScroll);
+        handleScroll();
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
-  return (
-    <div className="timeline">
-      {events.map((event, index) => (
-        <div key={index} className={`event ${index % 2 === 0 ? 'left' : 'right'}`}>
-          <div className="event-date">{event.date}</div>
-          <div className="event-title">{event.title}</div>
-          <div className="event-description">{event.description}</div>
+    return (
+        <div className="timeline">
+            {events.map((event, index) => (
+                <div
+                    key={index}
+                    className={`event ${index % 2 === 0 ? "left" : "right"}`}
+                >
+                    <h2 className="event-date">{event.date}</h2>
+                    <h3 className="event-title">{event.title}</h3>
+                    <div className="event-description">{event.description}</div>
+                    <h4>Keywords</h4>
+                    <ul style={{ listStyleType: "none", fontWeight: "100" }}>
+                        {event.keywords.map((word) => (
+                            <li
+                                style={{
+                                    display: "inline",
+                                }}
+                            >
+                                <a
+                                    href="#"
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "#071013",
+                                    }}
+                                >
+                                    &nbsp;&nbsp;&nbsp;
+                                    {word}
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Timeline;
